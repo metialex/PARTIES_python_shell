800ed99cc6f276c2c76825caf95e7a0140d9af19
metelkin <metialex@gmail.com>
2021-11-11 17:18:36 +0100

    updated

---
+++ examples/test.py
@@ -0,0 +1,10 @@
+import sys
+
+
+work_folder = "/home/vowinckel/applications/parties_shell/classes"
+sys.path.append(work_folder)
+from postprocess import post_process
+
+p = post_process()
+#p.plot_XY_vel_mag_contour('velocity_',8)
+p.plot_XY_vel_mag_contour('velocity_',1)
---
+++ readme.rd
@@ -0,0 +1,7 @@
+To create the enviroment which is needed to run use the classes from this repository,
+run the following commands:
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
