50d3a7579c125075793d20eda3d1037310d178c2
metelkin <metialex@gmail.com>
2021-11-16 17:57:27 +0100

    update

--- parties/tests/simulation_test.py
+++ parties/tests/simulation_test.py
@@ -1,4 +1,9 @@
-from parties.simulation import simulation
+try:
+    from parties.simulation import simulation
+    from parties.multiple_simulations import multiple_simulations
+    from post_process import post_process
+except:
+    print('Unable to import parties library')
 
 try:
     #test your functions here
