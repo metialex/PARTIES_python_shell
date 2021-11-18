5d5bf2f859e81a4a52dd1dbd9898e188c8fcd26e
metelkin <metialex@gmail.com>
2021-11-18 20:02:58 +0100

    update

--- parties/setup.py
+++ parties/setup.py
@@ -9,14 +9,14 @@ dependencies = ['numpy',
              
 setup(
    name='parties',
-   version='0.0.7',
+   version='0.0.9',
    description='A python shell for parties cfd code',
    license="MIT",
    long_description='A python shell for parties cfd code',
    author='Alexander Metelkin',
    author_email='a.metelkin@tu-braunschweig.de',
    url="https://github.com/metialex/PARTIES_python_shell",
-   py_modules=["parties.simulation", "parties.multiple_simulations", "parties.post_process"],
+   py_modules=["parties"],
    package_dir={'': 'src'},
    install_requires=dependencies #external packages as dependencies
 )
